480ce4fd821db53828856c92c95380dd5da706c9
Kris Fisher <kfisher1@graceland.edu>
2018-04-20 12:07:28 -0500

    website update

--- javascriptpractice1/info.js
+++ javascriptpractice1/info.js
@@ -1,6 +1,6 @@
-var name = "Kris Fisher";
+﻿var name = "Kris Fisher";
 
 document.write("<span id ='footer'>Kris's Website</span><br />");
 document.write("<span id ='copyright'>©2018 Kris Fisher</span><br />");
-document.write("<span id ='footer'>Modified Apr 16 2018</span><br />");
-
+document.write("<span id ='footer'>Modified Apr 20 2018</span><br />");
+document.write("<span id ='footer'>Retrieved: April 20 2018</span><br />");
